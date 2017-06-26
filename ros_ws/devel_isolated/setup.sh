7b9764fa896390c7666fd7b0ed9e706777b29406
Aramis Tanelus <bd543491@ahschool.com>
2017-06-26 07:53:04 -0400

    Cleaned it up

--- ros_ws/devel_isolated/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin.builder module
-
-. "/home/aramis/Desktop/Self-Driving-Car/ros_ws/devel_isolated/cartographer_rviz/setup.sh"
--- ros_ws/devel_isolated/setup.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin.builder module
-
-. "/home/aramis/Desktop/Self-Driving-Car/ros_ws/devel_isolated/cartographer_rviz/setup.zsh"
