7b1384eea60acf241a9695e2314978e7866e9948
geozhou29 <31490419+geozhou29@users.noreply.github.com>
2017-09-04 19:50:13 -0400

    Update UltrasonicSensor.cpp

--- ros/src/motors/UltrasonicSensor.cpp
+++ ros/src/motors/UltrasonicSensor.cpp
@@ -10,8 +10,8 @@ UltrasonicSensor::~UltrasonicSensor() {}
 
 double UltrasonicSensor::getDistance()
 {
-	int voltage = analogRead(sensorPin); //0 - 5V
-	double distance = voltage * 1000 * (4650.0 / 1023.0) + 350; //Converting 0 - 5V to 350mm - 5000mm
+	int voltage = analogRead(sensorPin); //0 - 1023
+	double distance = voltage * (4650.0 / 1023.0) + 350; //Converting 0 - 1023 to 350mm - 5000mm
 	return distance;
 }
 
